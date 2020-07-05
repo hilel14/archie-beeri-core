0c7bf11d6e7ee986b91ccb58ac92ed517b731de9
Hilel New <hilel14@gmail.com>
2020-07-05 07:10:26 +0300

    Change backup script to use different aws buckets

--- src/setup/backup.sh
+++ src/setup/backup.sh
@@ -12,7 +12,10 @@ curl http://localhost:8983/solr/archie_beeri/replication?command=backup
 aws s3 sync --only-show-errors --storage-class DEEP_ARCHIVE /opt/apache/solr/server/solr/archie_beeri/data/ s3://archie-beeri-backup/solr
 
 # Asset store
-aws s3 sync --only-show-errors --storage-class DEEP_ARCHIVE s3://archie-beeri-asset-store s3://archie-beeri-backup
+#aws s3 sync --only-show-errors --storage-class DEEP_ARCHIVE s3://archie-beeri-asset-store s3://archie-beeri-backup
+aws s3 sync --only-show-errors --storage-class DEEP_ARCHIVE s3://archie-beeri-private s3://archie-beeri-backup/private
+aws s3 sync --only-show-errors --storage-class DEEP_ARCHIVE s3://archie-beeri-pubilc s3://archie-beeri-backup/pubilc
+aws s3 sync --only-show-errors --storage-class DEEP_ARCHIVE s3://archie-beeri-secret s3://archie-beeri-backup/secret
 
 # Report done
 echo `date` >> /var/opt/archie/beeri/logs/backup.log 
