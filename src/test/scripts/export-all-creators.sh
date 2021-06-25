ca3670d5ebc78e269bca8aeff5e6a71f75679233
hilel <hilel@mdgo.io>
2021-06-25 12:27:30 +0300

    move scripts to onetimer project

--- src/test/scripts/export-all-creators.sh
+++
@@ -1,1 +0,0 @@
-curl "http://localhost:8983/solr/archie_beeri/select?fl=id%2CdcTitle%2CdcDate%2CdcCreator%2CdcDescription%2CdcSubject%2CstorageLocation%2CdcFormat%2CdcType%2CdcIsPartOf%2CdcAccessRights%2CimportTime&q=dcCreator%3A*&sort=id%20asc&start=0&rows=100000"
--- src/test/scripts/test-build.sh
+++
@@ -1,3 +0,0 @@
-mvn clean install
-mvn dependency:copy-dependencies
-rm target/dependency/log4j-slf4j-impl-2.11.2.jar
