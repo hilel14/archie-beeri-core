733ce18b255b4c861ab208a215253d68eb7f186d
hilel14 <hilel14@gmail.com>
2021-09-25 10:11:44 +0300

    add remark

--- src/main/scripts/re-index/6.import.sh
+++ src/main/scripts/re-index/6.import.sh
@@ -4,4 +4,4 @@
 curl 'http://localhost:8983/solr/archie_beeri/update/json/docs' -H 'Content-type:application/json' -d @import.json
 
 echo "\n"
-echo "verify that number of docs is the same as before"
+echo "Reload Solr core and verify that number of docs is the same as before"
